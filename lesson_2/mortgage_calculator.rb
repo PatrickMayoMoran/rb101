09373860ddb4a4c6557f1449d7429e4108feb8de
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-11-15 17:55:32 +0000

    add mortgage calculator comments/pseudo code

---
+++ lesson_2/mortgage_calculator.rb
@@ -0,0 +1,63 @@
+# Assignment
+# Create a mortgage loan calculator that asks for:
+# 1) the loan amount
+# 2) the Annual Percentage Rate (APR)
+# 3) the loan duration
+#
+# With these pieces of information, calculate and share:
+# 1) the monthly interest rate
+# 2) loan duration in months
+# 3) monthly payment
+#
+# Formula:
+#
+# monthly_payment = 
+# loan_amount * (monthly_interest / (1 - (1 + monthly_interest)**(-loan_duration_months)))
+
+# Helper methods needed
+# def prompt
+# valid_name?
+# valid_number?
+# format_loan_amount
+# loan_string_to_integer
+# move_to_next_calculation?
+#
+# Greet the user and ask for name
+# Validate name input
+# Greet user by name
+#
+# Start calculator
+# Prompt of information we will ask for
+#
+### Loan Amount
+# Ask for loan amount
+# Show examples of how to enter
+# Check for valid number
+# user confirms this is correct
+#
+### APR
+# Ask for APR as yearly rate
+# Show examples of how to enter
+# Check for valid input
+# user confirms this is correct
+#
+### Loan Duration
+# Ask for loan duration in years
+# Show examples of how to enter
+# Check for valid input
+# User confirms this is correct
+#
+### Calculating Payment
+# Tell user we are starting calculation
+# Show each piece of information being added
+# Display each piece of information calculated
+# -- monthly payment
+# -- loan duration in months
+# -- monthly interest rate
+#
+### End or redo
+# Thank user for calculating
+# Ask if they want to calculate again
+#
+### Farewell
+# Auf wiedersehen!
