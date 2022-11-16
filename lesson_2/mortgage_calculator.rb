1f92d2b19b695b1ae429d41e67e7bf84a686d9dc
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-11-16 15:43:22 +0000

    remove confirmation method typo

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -37,7 +37,7 @@ end
 
 def confirmation?(entry, units)
   prompt("You've entered #{entry} #{units}. Is this correct?")
-  prompt("Type 'yes' to continue; type anything else to reenter loan amount.")
+  prompt("Type 'yes' to continue; type anything else to reenter.")
   confirmation = Kernel.gets().chomp()
   return true if confirmation.downcase.start_with?('y')
 end
@@ -58,7 +58,7 @@ prompt("Hello #{name}! Let's calculate a mortgage!")
 # Start calculator
 # Prompt of information we will ask for
 prompt(
-<<~MSG
+<<-MSG
 We will need three pieces of information to calculate your mortgage:
 1) loan amount
 2) the Annual Percentage Rate (APR)
@@ -70,11 +70,11 @@ MSG
 prompt("Let's start with the loan amount.")
 # Show examples of how to enter
 prompt(
-<<~MSG
+<<-MSG
 Here are some examples:
-172,000
-2,045,000
-54,000
+  172,000
+  2,045,000
+  54,000
 MSG
 )
 
@@ -97,7 +97,7 @@ end
 apr = nil
 loop do
   # Show examples of how to enter
-  prompt(<<~MSG
+  prompt(<<-MSG
   Now we will enter your Annual Percentage Rate (aka an adjusted interest rate)
   Some examples of percentages are:
   1.5%
@@ -107,7 +107,7 @@ loop do
   )
   loop do
     # Ask for APR as yearly rate
-    prompt(<<~MSG
+    prompt(<<-MSG 
     Please enter your yearly APR as a whole number:
     For example, type 5 for 5% or 2.5 for 2.5%
     MSG
@@ -120,11 +120,28 @@ loop do
   # user confirms this is correct
   break if confirmation?(apr, "percent")
 end
+
 ### Loan Duration
-# Ask for loan duration in years
-# Show examples of how to enter
-# Check for valid input
-# User confirms this is correct
+loan_duration = nil
+loop do
+  # Ask for loan duration in years
+  prompt("Now you will enter the length of your loan term in years")
+  # Show examples of how to enter
+  prompt("Some examples are:
+    10
+    15
+    30
+    Your loan length in years:")
+  # Check for valid input
+  loop do
+    loan_duration = Kernel.gets().chomp()
+    break if valid_integer?(loan_duration)
+    prompt("Hmmm... that doesn't look right. Please enter a valid number")
+  end
+  # User confirms this is correct
+  break if confirmation?(loan_duration, "years")
+end
+
 #
 ### Calculating Payment
 # Tell user we are starting calculation
