ea98394fc7de433605a56677335264d7b36b8082
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-06 17:37:41 +0000

    add is Odd? problem alternative solutions

--- small_problems/easy_1/odd.rb
+++ small_problems/easy_1/odd.rb
@@ -8,3 +8,9 @@ puts is_odd?(-17)  # => true
 puts is_odd?(-8)   # => false
 puts is_odd?(0)    # => false
 puts is_odd?(7)    # => true
+
+# could also use:
+# (num % 2).abs == 1
+#
+# could also sub-in remainder
+# num.remainder(2).abs == 1
