0b78026ac0c90f98ff84b2632e9e36124718de63
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-07 19:09:02 +0000

    begin working on rock paper scissors

---
+++ lesson_2/rock_paper_scissors.rb
@@ -0,0 +1,21 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors'] 
+
+def prompt(message)
+  Kernel.puts("=>  #{message}")
+end
+
+loop do
+  prompt("Choose one: #{VALID_CHOICES.join(", ")}")
+  choice = Kernel.gets().chomp()
+
+  if VALID_CHOICES.include?(choice)
+    break
+  else
+    prompt("That's not a valid choice")
+  end
+
+computer_choice = VALID_CHOICES.sample
+
+
+
+
