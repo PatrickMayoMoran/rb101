64427c50f4528e7aa367d3dcaa7a48460e4db656
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-09 15:12:02 +0000

    refactor VALID CHOICES constant to use %w array creation

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,4 +1,4 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors'] 
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   Kernel.puts("=>  #{message}")
@@ -6,12 +6,12 @@ end
 
 def display_results(player, computer)
   if (player == 'rock' && computer == 'scissors') ||
-      (player == 'scissors' && computer == 'paper') ||
-      (player == 'paper' && computer == 'rock')
+     (player == 'scissors' && computer == 'paper') ||
+     (player == 'paper' && computer == 'rock')
     prompt("You won!")
   elsif (computer == 'rock' && player == 'scissors') ||
-      (computer == 'scissors' && player == 'paper') ||
-      (computer == 'paper' && player == 'rock')
+        (computer == 'scissors' && player == 'paper') ||
+        (computer == 'paper' && player == 'rock')
     prompt("Computer won!")
   else
     prompt("It's a draw!")
@@ -21,22 +21,22 @@ end
 loop do
   choice = nil
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(", ")}")
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
-  
+
     if VALID_CHOICES.include?(choice)
       break
     else
       prompt("That's not a valid choice")
     end
   end
-  
+
   computer_choice = VALID_CHOICES.sample
-  
+
   prompt("You chose #{choice}; computer chose #{computer_choice}")
   display_results(choice, computer_choice)
 
-prompt("do you want to play again?")
-answer = Kernel.gets().chomp()
-break unless answer.start_with?('y')
+  prompt("do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.start_with?('y')
 end
