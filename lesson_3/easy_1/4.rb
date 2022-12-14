ff010414911385246731255ee89a965fff871d4b
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-14 15:12:46 +0000

    add easy 1 exercise 4

---
+++ lesson_3/easy_1/4.rb
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+numbers.delete_at(1)
+# deletes at index 1, so will delete the value 2 from the array
+
+numbers = [1, 2, 3, 4, 5]
+numbers.delete(1)
+# deletes the value 1 where it is found, so will delete 1 at index 0
