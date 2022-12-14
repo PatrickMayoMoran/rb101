b42acb6a58873c9947b67c8801304c74d0eb02fb
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-14 17:02:21 +0000

    add hard exercise 2

---
+++ lesson_3/hard/2.rb
@@ -0,0 +1,9 @@
+greetings = { a: 'hi' }
+informal_greeting = greetings[:a]
+informal_greeting << ' there'
+
+puts informal_greeting  #  => "hi there"
+puts greetings
+
+# What is result of last line?
+# {:a=>"hi there"}
