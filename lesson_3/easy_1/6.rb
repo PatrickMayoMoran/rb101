49a277cee7b935cb14eb2dee85847f2b3932742f
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-14 15:21:42 +0000

    add easy 1 exercise 6

---
+++ lesson_3/easy_1/6.rb
@@ -0,0 +1,6 @@
+# Show two different ways to put expected "Four score and " in front
+famous_words = "seven years ago..."
+
+famous_words.prepend("Four score and ")
+
+famous_words = "Four score and " + famous_words
