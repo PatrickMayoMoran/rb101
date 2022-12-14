59e65e5d6b4f28c365056eb0096cba10ce9ebdd1
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-14 16:37:57 +0000

    add medium 1 exercise 3 explanations

--- lesson_3/medium_1/3.rb
+++ lesson_3/medium_1/3.rb
@@ -10,3 +10,10 @@ end
 
 puts factors(16)
 puts factors(0)
+
+# Purpose of number % divisor == 0 is checking that divisor is a factor 
+# with no remainder
+#
+# Purpose of line 8 is to return factors array
+#
+# problem was using begin /end loop instead of while
