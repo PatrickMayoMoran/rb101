a6705acd06e9645f2416acecee8acd6c55e3ebca
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-14 17:06:56 +0000

    add hard exercise 3

---
+++ lesson_3/hard/3.rb
@@ -0,0 +1,56 @@
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# output will be "one", "two", "three"
+# reassignment happens within method, original objects remain unchanged
+
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# output will be "one", "two", "three"
+# variables are reassigned within method, originals remain unchanged
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# output will be "two", "three", "one"
+# gsub! modifies original object
