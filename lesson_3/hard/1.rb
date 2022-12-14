e8b9e102d6b3170d5cf5fe53daaaefed17ccaf9b
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-14 17:00:25 +0000

    add hard exercise 1

---
+++ lesson_3/hard/1.rb
@@ -0,0 +1,8 @@
+if false
+  greeting = "hello world"
+end
+
+greeting
+
+# what do you expect from greeting being referenced on line 5?
+# nil - variables initialized in non-executed if statements will have nil value
