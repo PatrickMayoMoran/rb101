c5f108a876e52d5dcff4f80bdfc51379bde7f818
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-15 14:27:29 +0000

    add pedac example for second pedac practice problem

--- lesson_4/2_pedac_test.rb
+++ lesson_4/2_pedac_test.rb
@@ -30,11 +30,51 @@
 # Array of substrings
 #
 # ALGORITHM
+# if input string is empty, return empty array
+# create array of all substrings from string
+# loop through array, checking for palindromes
+#   if substring is a palindrome, append to empty array
+# return array
 #
-#
+# HELPER METHOD
+# Substrings
+# initialize results array
+# Set starting index of 0
+# # While starting index is less than or equal to string size minus 2
+# # set substring length to 2
+# #   While substring length + starting index is less than or equal to string size
+# #   slice string from starting index to sub length -1
+# #   append sliced to results array
+# #   increase substring length by 1
+# # increase starting index by 1
+# return results array
+def is_palindrome?(sub)
+  sub == sub.reverse
+end
+
+def substrings(str)
+  result = []
+  starting_index = 0
+  while starting_index + 2 <= str.length
+    sub_size = 2
+    while sub_size + starting_index <= str.length
+      sub = str.slice(starting_index, sub_size)
+      result << sub
+      sub_size += 1
+    end
+  starting_index += 1
+  end
+  result
+end
+
 def palindrome_substrings(str)
-  arr = []
-  return arr if str.empty?
+  palindromes = []
+  return palindromes if str.empty?
+  substrings = substrings(str)
+  substrings.each do |sub|
+    palindromes << sub if is_palindrome?(sub)
+  end
+  palindromes
 end
 
 p palindrome_substrings("supercalifragilisticexpialidocious") #== ["ili"]
