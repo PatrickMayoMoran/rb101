b4bd30c45a19329e7d43d67250d3162b127df726
Patrick Moran <patrick.mayo.moran@gmail.com>
2022-12-18 09:54:01 +0000

    add practice methods exercise 8

---
+++ lesson_4/methods_practice/1/8.rb
@@ -0,0 +1,5 @@
+# how does take work? Is it destructive? How can we find out?
+arr = [1, 2, 3, 4, 5]
+arr.take(2)
+# take returns the first n elements of self in a new array (non destructive)
+# We can find this out via testing and checking the documentation
