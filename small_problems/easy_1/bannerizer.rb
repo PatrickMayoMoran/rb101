3e455bf2d0d617ef79a8b69bc8cf7381a8c8c417
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-01-03 19:55:18 +0000

    add provided bannerizer solution from memory

--- small_problems/easy_1/bannerizer.rb
+++ small_problems/easy_1/bannerizer.rb
@@ -72,5 +72,16 @@ def print_in_box(str)
   puts box
 end
 
+def print_in_box(message)
+  horizontal = "+#{'-'*(message.size + 2)}+"
+  empty_line = "|#{' '*(message.size + 2)}|"
+
+  puts horizontal
+  puts empty_line
+  puts "| #{message} |"
+  puts empty_line
+  puts horizontal
+end
+
 print_in_box('To boldly go where no one has gone before.')
 print_in_box('')
