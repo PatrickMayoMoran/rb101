fec90492a3706fe128abd41d52d3e7672c61ad35
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-01-03 18:31:40 +0000

    add one liner alternative whats my bonus solution

--- small_problems/easy_1/whats_my_bonus.rb
+++ small_problems/easy_1/whats_my_bonus.rb
@@ -26,7 +26,14 @@ def calculate_bonus(salary, boolean)
   bonus = salary / 2
 end
 
+def calculate_bonus_2(salary, boolean)
+  boolean ? salary / 2 : 0
+end
+
 puts calculate_bonus(2800, true) == 1400
 puts calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
 
+puts calculate_bonus_2(2800, true) == 1400
+puts calculate_bonus_2(1000, false) == 0
+puts calculate_bonus_2(50000, true) == 25000
