66eefeabb33829a9ab7108f40c7ab35638de5c25
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-01-03 17:32:37 +0000

    add provided solution typed from memory

--- small_problems/easy_1/stringy_strings.rb
+++ small_problems/easy_1/stringy_strings.rb
@@ -46,7 +46,23 @@ def stringy(integer)
   string
 end
 
+def stringy_2(size, start=1)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
+  end
+
+  numbers.join
+end
+
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+puts stringy_2(6) == '101010'
+puts stringy_2(9) == '101010101'
+puts stringy_2(4) == '1010'
+puts stringy_2(7) == '1010101'
