222c39d1add66a30e49960b4bbc5684451379e2e
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-01-11 19:27:17 +0000

    add pedac for name greet exercise

---
+++ small_problems/easy_2/name_greet.rb
@@ -0,0 +1,31 @@
+# PROBLEM
+#   What is the problem?
+#     Greet a user by name, altering the greeting based on the punctuation that
+#     follows a user's name
+#   What is the goal
+#
+#   INPUT: none - but then name solicited from user
+#   OUTPUT: none - but then greeting output to screen
+#
+# EXAMPLES
+#
+#   EXPLICIT RULES
+#     greeting should change if name is followed by exclamation point
+#   IMPLICIT RULES
+#
+# DATA STRUCTURES
+#   How is the data organized?
+#   What structures might we use in our solution?
+#
+# ALGORITHM
+#   What steps will we use to solve the problem?
+#     prompt user for name
+#     check name for !
+#     if !, Big greet
+#     if not, small greet
+#     if ?, confusing greet
+#   What helper methods do we need? What will they do?
+#
+# CODE
+def greet
+end
