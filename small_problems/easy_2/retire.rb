89448b90f9556e6221d1f3e9d0d278b9627a0193
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-01-11 19:14:37 +0000

    complete pedac for retire problem

---
+++ small_problems/easy_2/retire.rb
@@ -0,0 +1,37 @@
+# PROBLEM
+#   What is the problem?
+#     Create a retirement calculator that solicits current age and desired
+#     retirement age, then outputs the current year, year someone will retire,
+#     and how many years to go
+#   What is the goal
+#
+#   INPUT: none initially - then a current age and desired retirement age
+#   OUTPUT: no return value - current year, desired year, and years to go
+#   displayed to the screen
+#
+# EXAMPLES
+#
+#   EXPLICIT RULES
+#   IMPLICIT RULES
+#     Whole age numbers
+#     display in years
+#
+# DATA STRUCTURES
+#   How is the data organized?
+#   What structures might we use in our solution?
+#
+# ALGORITHM
+#   What steps will we use to solve the problem?
+#     Prompt user for current age
+#     store current age
+#     Prompt user for desired retirement age
+#     store desired age
+#
+#     Display current year
+#     Calculate and display given retirement year
+#     display how many years to go
+#   What helper methods do we need? What will they do?
+#
+# CODE
+def retire
+end
