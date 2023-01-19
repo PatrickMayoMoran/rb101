b05775aaa298511a9331e04aad7a2e86f92f7fda
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-01-19 18:33:53 +0000

    add initial print odd numbers solution

---
+++ small_problems/easy_2/6_odd_numbers.rb
@@ -0,0 +1,21 @@
+# PROBLEM: Print all odd numbers from 1 to 99, inclusive, to the console, 
+# with each number on a separate line.
+#
+# INPUT
+# OUTPUT
+#
+# EXAMPLES
+# RULES
+#   inclusive, so 1 and 99 are also printed
+#   each number gets it's own line
+# DATA STRUCTURES
+#   range
+# ALGORITHM
+#   for each number in the range, 
+#     if number is odd, print to screen
+#     else, skip to next number
+# CODE
+
+(1..99).each do |number|
+  puts number if number.odd?
+end
